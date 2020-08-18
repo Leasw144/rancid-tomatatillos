d65d0a3d32a5cdbc4bb50bf1110426246a5a58ee
Sean Leas <phoenix_24leas.com>
2020-08-18 17:46:22 -0600

    Create CardSection class body #13

--- src/CardSection.js
+++ src/CardSection.js
@@ -1,7 +1,17 @@
 import React, {Component} from 'react'
 
 class CardSection extends Component {
-
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+  render() {
+    return (
+      <h3>I'm the card section</h3>
+    )
+  }
+  
 }
 
 export default CardSection
