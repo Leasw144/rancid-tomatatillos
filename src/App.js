d44fd6481216ca193d6bc8652c768bbd9e22207d
Sean Leas <phoenix_24leas.com>
2020-08-18 19:54:54 -0600

    Create fetch method resolve #12

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import './CSS/App.css';
 import Header from './Header'
 import CardSection from './CardSection'
 import './assets/tomato.jpg'
+
 class App extends Component {  
+  constructor() {
+    super();
+    this.state = {
+      movies: null
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(response => response.json())
+      .then(movies => this.setState({ movies }))
+      .then(movies => console.log(this.state))
+      .catch(error => console.log('Movie not found'))
+  }
+  
   render() {
     return (
       <main className="App">
         <Header />
-        <CardSection />              
+        <CardSection movies={this.state}/>              
       </main>
     )
   };
