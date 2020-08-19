70a430db036c2db763c6ec1a29b02ba93c5a52cf
Sean Leas <phoenix_24leas.com>
2020-08-18 18:14:38 -0600

    Fix paths to new css files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
-import './App.css';
+import './CSS/App.css';
 import Header from './Header'
 import CardSection from './CardSection'
-
+import './assets/tomato.jpg'
 class App extends Component {  
   render() {
     return (
       <main className="App">
-        <h1>Hello World</h1>
         <Header />
         <CardSection />              
       </main>
