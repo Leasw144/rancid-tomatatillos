983fb440a575d675a9d5e9dabf7027a529f1353b
Sean Leas <phoenix_24leas.com>
2020-08-18 21:17:10 -0600

    Comment out code in return

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -3,15 +3,15 @@ import '../CSS/MovieCard.css'
 
 
 function MovieCard(props) {
-  console.log('movie Card props', props)
-  return (
-    <article className='MovieCard'>
-      <img src='https://image.tmdb.org/t/p/original//sA154deR0X51EcR2lm2FfDczryg.jpg' alt='movie'/>
-      <p>Title:</p>
-      <p>Average Rating:</p>
-      <p>Release Date: </p>
-    </article>
-  )
+  // console.log('movie Card props', props)
+    return (
+      <article className='MovieCard'>
+        {/* <img src={movie.poster_path} alt={movie.title}/> */}
+        <p>Title: {props.title}</p>
+        {/* <p>Average Rating: {movie.average_rating}</p> */}
+        {/* <p>Release Date: {movie.release_date}</p> */}
+      </article>
+    )
 }
 
 export default MovieCard
