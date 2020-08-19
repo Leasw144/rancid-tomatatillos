89574fa2660d37e94e2448a5c438b63666a358b8
Naomi Ware <nware1066@yahoo.com>
2020-08-19 14:17:43 -0600

    moved fetch method to App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ import Header from './Components/Header'
 import CardSection from './Components/CardSection'
 import './assets/tomato.jpg'
 
-class App extends Component {  
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      movies: null
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(response => response.json())
+      .then(movies => this.setState({ movies }))
+      // .then(movies => console.log(this.state))
+      .catch(error => console.log('Movie not found'))
+  }
+
   // constructor() {
   //   super();
   //   this.state = {
@@ -19,12 +35,12 @@ class App extends Component {
   //     .then(movies => console.log(this.state))
   //     .catch(error => console.log('Movie not found'))
   // }
-  
+
   render() {
     return (
       <main className="App">
         <Header />
-        <CardSection/>              
+        <CardSection/>
       </main>
     )
   };
--- src/Components/CardSection.js
+++ src/Components/CardSection.js
@@ -1,20 +1,20 @@
 import React, {Component} from 'react'
 import MovieCard from './MovieCard'
 class CardSection extends Component {
-  constructor() {
-    super();
-    this.state = {
-      movies: null
-    }
-  }
-
-  componentDidMount() {
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-      .then(response => response.json())
-      .then(movies => this.setState({ movies }))
-      // .then(movies => console.log(this.state))
-      .catch(error => console.log('Movie not found'))
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     movies: null
+  //   }
+  // }
+  //
+  // componentDidMount() {
+  //   fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+  //     .then(response => response.json())
+  //     .then(movies => this.setState({ movies }))
+  //     // .then(movies => console.log(this.state))
+  //     .catch(error => console.log('Movie not found'))
+  // }
 
   render() {
     console.log('lives in card section', this.state.movies)
@@ -29,7 +29,7 @@ class CardSection extends Component {
       </section>
     )
   }
-  
+
 }
 
 export default CardSection
