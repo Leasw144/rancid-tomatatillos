bad11bf2d04ce9076242dc385178eaf14387ff3c
Sean Leas <phoenix_24leas.com>
2020-08-18 18:16:34 -0600

    Add elements to Header

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
+import './CSS/Header.css'
 
 function Header() {
   return (
-    <header>Hello Headerrrr</header>
+    <header>
+      <img src='./assets/tomato.jpg' alt='tomato' />
+      <h1>
+        Welcome! X
+      </h1>
+      <button type='button' className='login-page-button'>Log In</button>
+    </header>
   )
 }
 
