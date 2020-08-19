4b2430f9ed6e36d4811153f830e551fc4f521c9e
Sean Leas <phoenix_24leas.com>
2020-08-18 20:48:24 -0600

    Move state associated things to CardSection

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React, {Component} from 'react';
 import './CSS/App.css';
-import Header from './Header'
-import CardSection from './CardSection'
+import Header from './Components/Header'
+import CardSection from './Components/CardSection'
 import './assets/tomato.jpg'
 
 class App extends Component {  
-  constructor() {
-    super();
-    this.state = {
-      movies: null
-    }
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     movies: null
+  //   }
+  // }
 
-  componentDidMount() {
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-      .then(response => response.json())
-      .then(movies => this.setState({ movies }))
-      .then(movies => console.log(this.state))
-      .catch(error => console.log('Movie not found'))
-  }
+  // componentDidMount() {
+  //   fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+  //     .then(response => response.json())
+  //     .then(movies => this.setState({ movies }))
+  //     .then(movies => console.log(this.state))
+  //     .catch(error => console.log('Movie not found'))
+  // }
   
   render() {
     return (
       <main className="App">
         <Header />
-        <CardSection movies={this.state}/>              
+        <CardSection/>              
       </main>
     )
   };
