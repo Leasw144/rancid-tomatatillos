538eeffc94eafb13aa8a2ea35806f5bf6b4525e7
Sean Leas <phoenix_24leas.com>
2020-08-22 14:32:34 -0600

    Create beforeEach #25

--- src/CardSection/CardSection.test.js
+++ src/CardSection/CardSection.test.js
@@ -6,48 +6,62 @@ import App from '../App'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 describe('CardSection', () => {
-
-  
-  it('render a number of cards equal to the length of the array that is being passed in', () => {
-
-    const movies = [
-        {
-          "id": 1,
-          "poster_path": "https://image.tmdb.org/t/p/original//sA154deR0X51EcR2lm2FfDczryg.jpg",
-          "backdrop_path": "https://image.tmdb.org/t/p/original//juzEhsX92if2lJ2CSqKAI4RQswt.jpg",
-          "title": "Greenland",
-          "average_rating": 9,
-          "release_date": "2020-07-29"
-        },
-        {
-          "id": 2,
-          "poster_path": "https://image.tmdb.org/t/p/original//eDnHgozW8vfOaLHzfpHluf1GZCW.jpg",
-          "backdrop_path": "https://image.tmdb.org/t/p/original//u9YEh2xVAPVTKoaMNlB5tH6pXkm.jpg",
-          "title": "Archive",
-          "average_rating": 8.5,
-          "release_date": "2020-08-13"
-        },
-        {
-          "id": 3,
-          "poster_path": "https://image.tmdb.org/t/p/original//5KlRFKKSbyCiyYpZSS3A6G5bW0K.jpg",
-          "backdrop_path": "https://image.tmdb.org/t/p/original//qZ4NYuwME0j6QgJmIE6AZMgmCaj.jpg",
-          "title": "Akira",
-          "average_rating": 9,
-          "release_date": "1988-07-16"
-        }
-      ]
+  let movies, titleOne, titleTwo, titleThree, ratingOne, ratingTwo, ratingThree;
+  beforeEach(() => {
+    movies = [
+      {
+        "id": 1,
+        "poster_path": "https://image.tmdb.org/t/p/original//sA154deR0X51EcR2lm2FfDczryg.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//juzEhsX92if2lJ2CSqKAI4RQswt.jpg",
+        "title": "Greenland",
+        "average_rating": 9,
+        "release_date": "2020-07-29"
+      },
+      {
+        "id": 2,
+        "poster_path": "https://image.tmdb.org/t/p/original//eDnHgozW8vfOaLHzfpHluf1GZCW.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//u9YEh2xVAPVTKoaMNlB5tH6pXkm.jpg",
+        "title": "Archive",
+        "average_rating": 8.5,
+        "release_date": "2020-08-13"
+      },
+      {
+        "id": 3,
+        "poster_path": "https://image.tmdb.org/t/p/original//5KlRFKKSbyCiyYpZSS3A6G5bW0K.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//qZ4NYuwME0j6QgJmIE6AZMgmCaj.jpg",
+        "title": "Akira",
+        "average_rating": 9,
+        "release_date": "1988-07-16"
+      }
+    ]
 
     render(
-      <CardSection allMovies={movies}/>
+      <CardSection allMovies={movies} />
     )
-    
-    const titleOne = screen.getByText( "Greenland")
-    const titleTwo = screen.getByText( "Archive")
-    const titleThree = screen.getByText( "Akira")
+
+    titleOne = screen.getByText("Greenland")
+    titleTwo = screen.getByText("Archive")
+    titleThree = screen.getByText("Akira")
+
+    imageOne = screen.getByAltText('Greenland')
+    imageTwo = screen.getByAltText('Ar')
+    imageThree = screen.getByAltText('Greenland')
+
+
+  })
+
+  it('render a number of cards equal to the length of the array that is being passed in', () => {
 
     expect(titleOne).toBeInTheDocument()
     expect(titleTwo).toBeInTheDocument()
     expect(titleThree).toBeInTheDocument()
 
   })
+
+  it('should contain the ratings of all of the movies', () => {
+
+  expect(ratingOne).toEqual(9)
+  })
+
+
 })
