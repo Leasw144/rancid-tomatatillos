12ffe724bc879e8d6bc6b3dc04eaddbe603f4059
Sean Leas <phoenix_24leas.com>
2020-08-23 12:48:15 -0600

    Add id prop to pass down to MovieCard

--- src/CardSection/CardSection.js
+++ src/CardSection/CardSection.js
@@ -3,12 +3,15 @@ import MovieCard from '../MovieCard/MovieCard'
 import './CardSection.css'
 
 function CardSection(props) {
+
   let movies;
   if (props.allMovies.length > 0) {
+    console.log(props)
     movies = props.allMovies.map(movie => {
       return (
         <MovieCard
         key={movie.id}
+        id={movie.id}
         img={movie.poster_path}
         title={movie.title}
         releaseDate={movie.release_date}
