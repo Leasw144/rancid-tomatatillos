d974c883f6a680d9dd2a9caebcd8a258b626f454
Sean Leas <phoenix_24leas.com>
2020-08-23 14:07:30 -0600

    Set up jsx for description #34

--- src/DetailsPage/DetailsPage.js
+++ src/DetailsPage/DetailsPage.js
@@ -1,3 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
+import './DetailsPage.css'
 
-import './DetailsPage'
+function DetailsPage(props) {
+  console.log(props)
+  return (
+    <section className='DetailsPage'>
+      <h1 className='movie-title'>Lord of Rings</h1>
+      <img src='' alt='Picture of Frodo' />
+      <article>
+        <h2>Description:</h2>
+        {/* <p>Release Date: 12/12/2002</p> */}
+        {/* <p>Overview: ksadfjalkdfjafklj</p> */}
+        {props.overview > 0 && <p> overview: {props.overview}</p>}
+        {props.release_date > 0 && <p> Release Date: {props.release_date}</p>}
+        {props.runtime > 0 && <p> runtime: {props.runtime}</p>}
+        {props.average_rating > 0 && <p> Average Rating: {props.average_rating}</p>}
+        {props.budget > 0 && <p> Budget: {props.budget}</p>}
+        {props.revenue > 0 && <p> Revenue: {props.revenue}</p>}
+        {props.tagline > 0 && <p> Budget: {props.tagline}</p>}
+      </article>
+      
+    </section>
+  )
+}
