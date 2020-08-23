943e96307ff6951bf4d80e79b57be08bc5374f63
Sean Leas <phoenix_24leas.com>
2020-08-23 13:38:06 -0600

    Create show info function #34

--- src/App.js
+++ src/App.js
@@ -40,14 +40,25 @@ class App extends Component {
   getUser = (username, password)  => {
     this.authorizeUser(username, password)
   }
+  
+  showInfo = (id) =>{
+    console.log('this function has been passed down successfully, bro', id)
+  }
+  // Create a function in app that invokes our API to go to the movie
+  // Pass this function down to MovieCard
+  // then OnClick of the card, the id would be fed into the function which calls the fetch
+  // // the fetch resets the state
+  // Have conditional logic hear, where if the movieInfo state is truthy, render details page
+  // pass down movie info as prop to details page
+
 
   render() {
     return (
       <main className="App">
         <Header loginPage={this.handleClick} logoutUser={this.logoutUser} user={this.state.user}/>
-          {this.state.isLogInShowing ? <Login getUser={this.getUser} /> : <CardSection allMovies= {this.state.movies} />}
+          {this.state.isLogInShowing ? <Login getUser={this.getUser} /> : <CardSection allMovies= {this.state.movies} showInfo={this.showInfo}/>}
           {this.state.error && <p className='error-msg'>{this.state.error}</p>}
-          {this.state.movieInfo.title && <DetailsPage movieInfo={this.state.movieInfo}/>}
+          {/* {this.state.movieInfo.title && <DetailsPage movieInfo={this.state.movieInfo}/>} */}
       </main>
     )
   };
