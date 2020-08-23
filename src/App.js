17b2ae9eeae7abd0742a655f240b07194116962c
Horacio Borrego <hborrego365@gmail.com>
2020-08-22 20:18:03 -0600

    Add ability to change view based on user login state

--- src/App.js
+++ src/App.js
@@ -31,6 +31,11 @@ class App extends Component {
 
   handleClick = () => {
     this.setState(prevState => ({isLogInShowing: !prevState.isLogInShowing}))
+   
+  }
+
+  logoutUser = () => {
+    this.setState(prevState => ({isLoggedIn: !prevState.isLoggedIn, user:{}}))
   }
 
   getUser = (username, password)  => {
@@ -55,9 +60,9 @@ class App extends Component {
   render() {
     return (
       <main className="App">
-      <Header handleClick={this.handleClick} user={this.state.user}/>
-      {this.state.isLogInShowing ? <Login getUser={this.getUser} /> : <CardSection allMovies={this.state.movies} />}
-      {this.state.error && <p className='error-msg'>{this.state.error}</p>}
+        <Header loginPage={this.handleClick} logoutUser={this.logoutUser} user={this.state.user}/>
+          {this.state.isLogInShowing ? <Login getUser={this.getUser} /> : <CardSection allMovies= {this.state.movies} />}
+          {this.state.error && <p className='error-msg'>{this.state.error}</p>}
       </main>
     )
   };
