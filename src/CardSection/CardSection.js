5db22b3cb867d347dbfd5a24bd8d0f33585167bd
Sean Leas <phoenix_24leas.com>
2020-09-01 14:22:40 -0600

    Add button functionality for favorite button

--- src/CardSection/CardSection.js
+++ src/CardSection/CardSection.js
@@ -5,13 +5,14 @@ import { Link, render } from 'react-browser-router'
 import DetailsPage from '../DetailsPage/DetailsPage'
 
 function CardSection(props) {
-  let movies;
+  console.log('Your favs', props)
+  let movies, favoriteMovie;
   if (props.allMovies.length > 0) {
     movies = props.allMovies.map(movie => {
+      favoriteMovie = props.favorites.find(moviesFavoritedId => moviesFavoritedId === movie.id)
       return (
         <div>
-          
-          <button type='button'>'♡'</button>
+          <button type='button' onClick={() => props.toggleFavorite(movie.id)}>{favoriteMovie ? '♥️' :'♡'}</button>
           <Link  key={movie.id} to={`/movies/${movie.id}`} >
             {/* render={DetailsPage} */}
             <MovieCard
@@ -35,9 +36,9 @@ function CardSection(props) {
   }
   return (
     <main>
-      <nav>
-        {props.userInfo.name && <button type='button'>filter</button>}
-      </nav>
+      <Link to={'/favorites'}>
+        {props.userInfo.name && <button type='button' onClick={props.favoriteMovies}>filter</button>}
+      </Link>
       <section className='card-section'>
         {movies}
       </section>
